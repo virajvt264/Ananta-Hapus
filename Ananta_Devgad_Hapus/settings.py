aaefc7e0d9693be584d72c8d7cd957e5550bcb3c
viraj tukarul <120440771+virajvt264@users.noreply.github.com>
2025-03-21 14:41:57 +0530

    Describe the changes you made

--- Ananta_Devgad_Hapus/settings.py
+++ Ananta_Devgad_Hapus/settings.py
@@ -124,8 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 # Default primary key field type

62e943a1257e0234f1aed686840af45af8b764b0
viraj tukarul <120440771+virajvt264@users.noreply.github.com>
2025-03-22 14:30:55 +0530

    Describe the changes you made

--- mango_shop/models.py
+++ mango_shop/models.py
@@ -7,11 +7,11 @@ class Product(models.Model):
     product_id =models.AutoField
     product_name = models.CharField(max_length=50)
     category = models.CharField(max_length=50, default="")
-    subcategory = models.CharField(max_length=50, default="")
+    small_desc = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
     description = models.CharField(max_length=200)
     pub_date = models.DateField()
-    image = models.ImageField(upload_to="mango_shop/images", default="")
+    image_url = models.URLField(max_length=500, blank=True, null=True)
 
     def __str__(self):
         return self.product_name
